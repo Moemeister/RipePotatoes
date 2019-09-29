e625397f835b5c99a1d5c8b05d0a4052f75c57b0
Moemeister <00062716@uca.edu.sv>
2019-09-29 11:00:31 -0600

    Initial Commit

---
+++ src/main/java/com/proyecto/ripepotatoes/RipepotatoesApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.ripepotatoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RipepotatoesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RipepotatoesApplication.class, args);
+    }
+
+}
