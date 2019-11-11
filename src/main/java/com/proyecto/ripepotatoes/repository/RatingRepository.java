5ff0f2a3364c07f6907fef5d93bd9c927fad2ad1
00010216 <00010216@uca.edu.sv>
2019-11-10 21:40:25 -0600

    Se creo query para calcular porcentaje y validacion de resultado

--- src/main/java/com/proyecto/ripepotatoes/repository/RatingRepository.java
+++ src/main/java/com/proyecto/ripepotatoes/repository/RatingRepository.java
@@ -3,9 +3,16 @@ package com.proyecto.ripepotatoes.repository;
 import com.proyecto.ripepotatoes.domain.Rating;
 import com.proyecto.ripepotatoes.domain.RatingDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RatingRepository extends JpaRepository<Rating, Integer> {
     public Rating findByIdPeliApiAndIdUsuario(Integer idpeli, Integer iduser);
+
+    @Query(value = "select ((avg(rating))/(5))*100 from rating where id_peli = ?1",
+            nativeQuery = true)
+    public Double getRatingPercentage(Integer id_peli);
+
+    public Integer countByIdPeliApi(Integer id_peli);
 }
