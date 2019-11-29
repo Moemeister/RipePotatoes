7734223f4af97bb67296cae4f2748a67429e65b5
00010216 <00010216@uca.edu.sv>
2019-11-29 17:14:06 -0600

    Unit test for catalog movie list
    Co-Authored-By: Kevin López <00251716@users.noreply.github.com>

---
+++ src/test/java/com/proyecto/ripepotatoes/testing/MoviesCatalogTest.java
@@ -0,0 +1,34 @@
+package com.proyecto.ripepotatoes.testing;
+
+import com.proyecto.ripepotatoes.domain.Rating;
+import com.proyecto.ripepotatoes.models.MovieDetails;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+
+public class MoviesCatalogTest {
+
+    @Value("${api.key}")
+    private String apiKey;
+    Integer id_peli = 475557;
+    RestTemplate restTemplate = new RestTemplate();
+    public RestTemplate getRestTemplate() {
+        return restTemplate;
+    }
+
+    @Test
+    public void fetchMoviefromAPITest() {
+        String url = "https://api.themoviedb.org/3/movie/{id_peli}?api_key="+apiKey+"&language=es-ES";
+        MovieDetails movie = restTemplate.getForObject(url,MovieDetails .class,id_peli);
+
+        assertThat(movie).isNotNull();
+    }
+}
