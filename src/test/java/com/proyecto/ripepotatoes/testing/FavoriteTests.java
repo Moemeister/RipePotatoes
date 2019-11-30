8a4540b5c340e7fed410673c65a1030d237c9b7d
00010216 <00010216@uca.edu.sv>
2019-11-29 20:22:51 -0600

    favorite test created need to improve
    Co-Authored-By: Kevin López <00251716@users.noreply.github.com>

---
+++ src/test/java/com/proyecto/ripepotatoes/testing/FavoriteTests.java
@@ -0,0 +1,62 @@
+package com.proyecto.ripepotatoes.testing;
+
+import com.proyecto.ripepotatoes.domain.Rol;
+import com.proyecto.ripepotatoes.domain.Usuario;
+import com.proyecto.ripepotatoes.repository.RolRepository;
+import com.proyecto.ripepotatoes.service.FavoriteService;
+import com.proyecto.ripepotatoes.service.UsuarioService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FavoriteTests {
+
+    @Autowired
+    BCryptPasswordEncoder encoder;
+
+    @Autowired
+    RolRepository rolRepository;
+
+    @Autowired
+    UsuarioService usuarioService;
+
+    @Autowired
+    FavoriteService favoriteService;
+
+    @Test
+    public void addFavoriteMovieTest() {
+
+        Integer id_peli = 475557;
+        Usuario user = userTest();
+        boolean favoriteExists = favoriteService.isFavoriteAlreadyAdded(id_peli,user);
+        assertThat(favoriteExists).isFalse();
+        favoriteService.save(user,id_peli);
+        assertThat(favoriteExists).isTrue();
+        usuarioService.remove(user);
+    }
+
+    public Usuario userTest(){
+        Usuario usuario= new Usuario();
+        //Rol rol = new Rol();
+        usuario.setNombre("dTester");
+        usuario.setApellido("dTester");
+        usuario.setUsername("d");
+        usuario.setStatus("VERIFIED");
+        usuario.setPass(encoder.encode("154"));
+        Rol rol = rolRepository.findByRol("USER");
+        usuario.setRoles(new HashSet<Rol>(Arrays.asList(rol)));
+        usuarioService.save(usuario);
+        Usuario userTest = usuarioService.findByUsername("d");
+
+        return userTest;
+    }
+}
